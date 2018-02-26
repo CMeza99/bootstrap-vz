2a39cc002328cb8d969a7fa077c267955e1b7f52
Carlos Meza <carlos.meza@verizondigitalmedia.com>
2018-02-26 00:38:22 +0000

    add ability to declare defaults options out of code

--- bootstrapvz/common/releases.py
+++ bootstrapvz/common/releases.py
@@ -1,12 +1,13 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractproperty
 
 
 class _Release(object):
     __metaclass__ = ABCMeta
 
-    def __init__(self, codename, version):
+    def __init__(self, codename, version, defaults=None):
         self.codename = codename
         self.version = version
+        self.defaults = defaults if defaults is not None else dict()
 
     def __cmp__(self, other):
         return self.version - other.version
@@ -23,24 +24,33 @@ class _Release(object):
         for key in state:
             self.__dict__[key] = state[key]
 
-    # TODO: add metaproperty disto_name
+    @abstractproperty
+    def distro(self):
+        pass
 
 
 class ReleaseDebian(_Release):
-    pass
+    @property
+    def distro(self):
+        return 'debian'
 
 
 class ReleaseDerivate(_Release):
-    def __init__(self, codename, version, debian):
+    def __init__(self, codename, version, defaults=None, distro=None, debian=None):
+        self._distro = distro
         self.debian = debian
-        super(ReleaseDerivate, self).__init__(codename, version)
+        super(ReleaseDerivate, self).__init__(codename, version, defaults)
 
     def __cmp__(self, other):
         if isinstance(other, ReleaseDebian):
             return self.debian.version - other.version
         elif not isinstance(other, ReleaseDerivate):
-            raise UnknownReleaseException('Unable to compare release with {}'.format(type(other)))
+            raise UnknownReleaseException('Unable to compare release with {}'.format(other))
         return self.version - other.version
+
+    @property
+    def distro(self):
+        return self._distro
 
 
 class _ReleaseAlias(_Release):
@@ -52,6 +62,10 @@ class _ReleaseAlias(_Release):
     def __str__(self):
         return self.alias
 
+    @property
+    def distro(self):
+        return self.release.distro
+
 
 def get_release(release_name, distro_name=None):
     from .tools import config_get, rel_path
@@ -61,6 +75,7 @@ def get_release(release_name, distro_name=None):
     release_info = dict()
     for distro in distros:
         release_info_tmp = release_map[distro].get(release_name, None)
+
         if not release_info_tmp:
             continue
         elif isinstance(release_info_tmp, dict):
@@ -69,15 +84,28 @@ def get_release(release_name, distro_name=None):
             release_info['version'] = release_info_tmp
         break
 
-    if release_info is None or not distro:  # pylint: disable=undefined-loop-variable
+    if not release_info or not distro:  # pylint: disable=undefined-loop-variable
         raise UnknownReleaseException(
             'The release `{name}\' is unknown'.format(name=release_name))
-    elif 'alias' in release_info:
-        return _ReleaseAlias(release_name, get_release(release_info['alias']))
-    elif distro.lower() != 'debian':  # pylint: disable=undefined-loop-variable
-        return ReleaseDerivate(release_name, release_info['version'], get_release(release_info['debian']))
 
-    return ReleaseDebian(release_name, release_info['version'])
+    if 'alias' in release_info:
+        return _ReleaseAlias(release_name, get_release(release_info['alias']))
+    else:
+        # update distro defaults with release defaults
+        defaults_tmp = release_map[
+            distro].get('_defaults', {}).copy()  # pylint: disable=undefined-loop-variable
+        defaults_tmp.update(release_info.get('_defaults', {}))
+        release_info['_defaults'] = defaults_tmp
+
+    if distro.lower() != 'debian':  # pylint: disable=undefined-loop-variable
+        return ReleaseDerivate(
+            release_name,
+            release_info['version'],
+            release_info['_defaults'],
+            distro,  # pylint: disable=undefined-loop-variable
+            get_release(release_info['debian']))
+
+    return ReleaseDebian(release_name, release_info['version'], release_info['_defaults'])
 
 
 class UnknownReleaseException(Exception):
